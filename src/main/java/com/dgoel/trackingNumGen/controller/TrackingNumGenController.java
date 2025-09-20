a34a0f38a78d8d4af864f596ceb7bb0600069e33
dgoel <dgoel@dgoels-MacBook-Pro.local>
2025-09-21 05:08:36 +0530

    Added sample API

--- src/main/java/com/dgoel/trackingNumGen/controller/TrackingNumGenController.java
+++ src/main/java/com/dgoel/trackingNumGen/controller/TrackingNumGenController.java
@@ -18,7 +18,17 @@ public class TrackingNumGenController {
         this.trackingNumGenService = trackingNumGenService;
     }
 
-    // curl "http://localhost:8080/next-tracking-number?origin_country_id=MY&destination_country_id=ID&weight=1.234&created_at=2018-11-20T19:29:32%2B08:00&customer_id=de619854-b59b-425e-9db4-943979e1bd49&customer_name=RedBox%20Logistics&customer_slug=redbox-logistics"
+    @GetMapping
+    public String sampleAPI() {
+        return "Sample API: <a href=" +
+                "'https://trackingnumgen.onrender.com/next-tracking-number?origin_country_id=MY&destination_country_id=ID&weight=1.234&created_at=2018-11-20T19:29:32%2B08:00&customer_id=de619854-b59b-425e-9db4-943979e1bd49&customer_name=RedBox%20Logistics&customer_slug=redbox-logistics'" +
+                " target='_blank' style='display:inline-block; padding:10px 20px; background-color:#4CAF50; " +
+                "color:white; text-decoration:none; border-radius:5px;'>Click Here</a> <br> <br> <br> " +
+                "Above URL as text: " +
+                "https://trackingnumgen.onrender.com/next-tracking-number?origin_country_id=MY&destination_country_id=ID&weight=1.234&created_at=2018-11-20T19:29:32%2B08:00&customer_id=de619854-b59b-425e-9db4-943979e1bd49&customer_name=RedBox%20Logistics&customer_slug=redbox-logistics";
+    }
+
+    // /next-tracking-number?origin_country_id=MY&destination_country_id=ID&weight=1.234&created_at=2018-11-20T19:29:32%2B08:00&customer_id=de619854-b59b-425e-9db4-943979e1bd49&customer_name=RedBox%20Logistics&customer_slug=redbox-logistics
     @GetMapping("/next-tracking-number")
     public Map<String, String> getNextTrackingNumber(
             @RequestParam("origin_country_id") String originCountryId,
